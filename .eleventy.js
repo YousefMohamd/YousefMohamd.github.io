46f9a9989a97493e60962a9056a0763d17f25104
yousef mohamed <mohamedyou1357@gmail.com>
2025-08-27 00:14:10 +0300

    Fix Eleventy CI: lock version, simple .eleventy.js, workflow uses npm run build

--- .eleventy.js
+++ .eleventy.js
@@ -1,111 +1,14 @@
-const { DateTime } = require("luxon");
-const fs = require("fs");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginNavigation = require("@11ty/eleventy-navigation");
-const markdownIt = require("markdown-it");
-const markdownItAnchor = require("markdown-it-anchor");
-
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPlugin(pluginSyntaxHighlight);
-  eleventyConfig.addPlugin(pluginNavigation);
+  // مرّر الملفات الساكنة زي ما هي
+  eleventyConfig.addPassthroughCopy({ "img": "img" });
+  eleventyConfig.addPassthroughCopy({ "css": "css" });
+  eleventyConfig.addPassthroughCopy({ "files": "files" });
 
-  eleventyConfig.setDataDeepMerge(true);
-
-  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
-
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
-  });
-
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
-  });
-
-  // Get the first `n` elements of a collection.
-  eleventyConfig.addFilter("head", (array, n) => {
-    if( n < 0 ) {
-      return array.slice(n);
-    }
-
-    return array.slice(0, n);
-  });
-
-  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
-
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("files");
-  eleventyConfig.addPassthroughCopy("css");
-
-  /* Markdown Overrides */
-  let markdownLibrary = markdownIt({
-    html: true,
-    breaks: true,
-    linkify: true
-  }).use(markdownItAnchor, {
-    permalink: true,
-    permalinkClass: "direct-link",
-    permalinkSymbol: "#"
-  });
-  eleventyConfig.setLibrary("md", markdownLibrary);
-
-  // Browsersync Overrides
-  eleventyConfig.setBrowserSyncConfig({
-    callbacks: {
-      ready: function(err, browserSync) {
-        const fs = require("fs");
-eleventyConfig.setBrowserSyncConfig({
-  callbacks: {
-    ready: function(err, bs) {
-      try {
-        const content404 = fs.readFileSync("_site/404.html");
-        bs.addMiddleware("*", (req, res) => {
-          res.writeHead(404, {"Content-Type": "text/html; charset=UTF-8"});
-          res.end(content404);
-        });
-      } catch (e) {
-        // لو 404 لسه متبنّاش، تجاهل بدل ما نكراش
-        console.warn("404.html not found yet; skipping custom 404 middleware.");
-      }
-    }
-  }
-});
-      },
-    },
-    ui: false,
-    ghostMode: false
-  });
-
+  // إعدادات القوالب
   return {
-    templateFormats: [
-      "md",
-      "njk",
-      "html",
-      "liquid"
-    ],
-
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about those.
-
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for link URLs (it does not affect your file structure)
-    // Best paired with the `url` filter: https://www.11ty.io/docs/filters/url/
-
-    // You can also pass this in on the command line using `--pathprefix`
-    // pathPrefix: "/",
-
-    markdownTemplateEngine: "liquid",
+    dir: { input: ".", includes: "_includes", data: "_data", output: "_site" },
     htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
-
-    // These are all optional, defaults are shown:
-    dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
+    markdownTemplateEngine: "njk",
+    templateFormats: ["njk","md","html"]
   };
 };
